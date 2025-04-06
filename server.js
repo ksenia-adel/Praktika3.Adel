06eb307d87c3a23efae12896af4dc0f374425e7f
Ksenia Adel <kseniaadel@gmail.com>
2025-04-06 22:42:13 +0300

    config.json removed

--- server.js
+++ server.js
@@ -37,11 +37,11 @@ app.use('/api/logs', activityRoutes);
 
 // health check
 app.get('/', function (req, res) {
-  res.send('📚 BOOKS API is running');
+  res.send('BOOKS API is running');
 });
 
 // start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
